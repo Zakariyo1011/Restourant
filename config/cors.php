440da9fe5f766c3207317ebe237571f91ca2e07f
Zakariyo1011 <zhacker1011z@gmail.com>
2026-05-13 15:48:38 +0500

    Fix CORS for production

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'https://restourant-front.vercel.app',
+],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
