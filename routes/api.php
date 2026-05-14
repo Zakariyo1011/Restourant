1eb7e28470072840547892df17aee936c170f806
Zakariyo1011 <zhacker1011z@gmail.com>
2026-05-14 15:21:48 +0500

    Temporarily remove nearby route

--- routes/api.php
+++ routes/api.php
@@ -64,36 +64,3 @@ Route::get('/meta/cuisine-types', function () {
 
 
 
-// Yaqin restoranlar (GPS bo'yicha)
-Route::get('/restaurants/nearby', function (Illuminate\Http\Request $request) {
-    $lat = $request->query('lat');
-    $lng = $request->query('lng');
-    $radius = $request->query('radius', 50);
-
-    if (!$lat || !$lng) {
-        return response()->json(['message' => 'Koordinatalar kerak'], 400);
-    }
-
-    $restaurants = \App\Models\Restaurant::with('location')
-        ->where('is_active', true)
-        ->whereHas('location')
-        ->get()
-        ->map(function ($restaurant) use ($lat, $lng) {
-            $R = 6371;
-            $dLat = deg2rad($restaurant->location->latitude - $lat);
-            $dLon = deg2rad($restaurant->location->longitude - $lng);
-            $a = sin($dLat/2) * sin($dLat/2) +
-                 cos(deg2rad($lat)) * cos(deg2rad($restaurant->location->latitude)) *
-                 sin($dLon/2) * sin($dLon/2);
-            $c = 2 * atan2(sqrt($a), sqrt(1-$a));
-            $restaurant->distance = round($R * $c, 1);
-            return $restaurant;
-        })
-        ->filter(fn($r) => $r->distance <= $radius)
-        ->sortBy('distance')
-        ->values();
-
-    return response()->json($restaurants);
-});
-
-//TESTLASH //////tgit 
